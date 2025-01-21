97b6843426ce29d8dd1496c8fc7fdf15cd0e70b3
awais_javaid <awais@devtechx.com>
2025-01-21 17:23:53 +0400

    fix CROS

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,19 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'http://127.0.0.1:5173',
+        'http://localhost:5173',
+        'http://127.0.0.1:8000',
+        'https://businessexperts.ae',
+        'https://admin.businessexperts.ae'
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +37,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
