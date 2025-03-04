3f4a4f4a00f13f5dced0a6a680a8e9cbaad8dad5
awais_javaid <awais@devtechx.com>
2025-03-04 15:18:50 +0400

    contries api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ContactUsController;
 use App\Http\Controllers\VisitorsController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +19,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/countries', function () {
+    $response = Http::get('https://restcountries.com/v3.1/all?fields=name,cca2');
+
+    if ($response->failed()) {
+        return response()->json(['error' => 'Failed to fetch countries'], 500);
+    }
+
+    return response()->json($response->json());
 });
 
 Route::post('contact-us', [ContactUsController::class, 'submit_contact_us_form']);
